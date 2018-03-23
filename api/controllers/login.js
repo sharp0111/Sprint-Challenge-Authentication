99096ec8079fec491f87d68397762de74c6d803a
Res Yeg <shaqk702@gmail.com>
2018-03-23 08:53:28 -0700

    Step 3 complete

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -4,7 +4,8 @@ const User = require('../models/userModels');
 
 const login = (req, res) => {
   const { username, password } = req.body;
-  User.findOne({ username }, (err, user) => {
+  const usernameLowerCase = username.toLowerCase();
+  User.findOne({ username: usernameLowerCase }, (err, user) => {
     if (err) {
       res.status(403).json({ error: 'Invalid Username/Password' });
       return;
