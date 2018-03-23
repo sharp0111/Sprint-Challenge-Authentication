8ae724790e43f5d583c2fd8005ad2b29cbddf9a7
Res Yeg <shaqk702@gmail.com>
2018-03-23 08:47:29 -0700

    Step 2 completed

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,14 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  const user = new User({ username, password });
+  user.save((err, user) => {
+    if (err) return res.send(err);
+    res.json({
+      success: 'User saved',
+      user
+    });
+  });
 };
 
 module.exports = {
